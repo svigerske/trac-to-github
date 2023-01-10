78a0a9ef3fd159a61f5eb56a77756874f684a79e
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-01-10 15:38:22 -0800

    rsync-archive-to-ghe-and-import.sh: Make GUID, TARGET_REPO, REMOTE_WORK_DIR configurable in SECRETS

--- rsync-archive-to-ghe-and-import.sh
+++ rsync-archive-to-ghe-and-import.sh
@@ -2,18 +2,26 @@
 set -eu
 . ./SECRETS
 set -x
-# We start a fresh migration, keyed to current time
-GUID=cae4a192-8fed-11ed-84b0-$(date +"%y%m%d%H%M%S")
-# Freshly named repository for each migration attempt
-sed -e "s/@HOST_IP@/$HOST_IP/;s/@TARGET_REPO@/sage-$(date +"%Y%m%d%H%M%S")/" map-ghe.csv.in > map-ghe.csv
+if [ -z "${GUID-}" ]; then
+    # We start a fresh migration, keyed to current time
+    GUID=cae4a192-8fed-11ed-84b0-$(date +"%y%m%d%H%M%S")
+fi
+if [ -z "${TARGET_REPO-}" ]; then
+    # Freshly named repository for each migration attempt
+    TARGET_REPO=sage-$(date +"%Y%m%d%H%M%S")
+fi
+sed -e "s/@HOST_IP@/$HOST_IP/;s/@TARGET_REPO@/$TARGET_REPO/" map-ghe.csv.in > map-ghe.csv
 RSYNC_OPTIONS=--stats
 #RSYNC_OPTIONS=--verbose
-rsync $RSYNC_OPTIONS --delete --rsh='ssh -p122' -a archive map-ghe.csv admin@$HOST_IP:
+if [ -z "${REMOTE_WORK_DIR-}" ]; then
+    REMOTE_WORK_DIR=.
+fi
+rsync $RSYNC_OPTIONS --delete --rsh='ssh -p122' -a archive map-ghe.csv admin@$HOST_IP:$REMOTE_WORK_DIR
 # import
 time ssh -p 122 admin@$HOST_IP "set -x;
-    (cd archive && tar cfz - .) > archive.tgz &&
+    (cd $REMOTE_WORK_DIR/archive && tar cfz - .) > archive.tgz &&
     ghe-migrator prepare archive.tgz -g $GUID &&
-    ghe-migrator map -i map-ghe.csv -g $GUID &&
+    ghe-migrator map -i $REMOTE_WORK_DIR/map-ghe.csv -g $GUID &&
     if ghe-migrator import archive.tgz -g $GUID  -u mkoeppe -p $IMPORT_TOKEN; then
         ghe-migrator audit -g $GUID;
         ghe-migrator unlock -g $GUID;
