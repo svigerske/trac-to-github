041a381fbd3b8f8433651148eda84dd5b1966624
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-02-11 11:30:59 -0800

    write_minimizing_queries.py: Don't forget to write out the last batch

--- write_minimizing_queries.py
+++ write_minimizing_queries.py
@@ -8,11 +8,19 @@ with open("minimized_issue_comment_node_ids.json", "r") as f:
     node_ids = json.load(f)
 
 query = ""
+batch = 0
+
+def dump_batch():
+    with open(f"minimizing_query_{batch}.json", "w") as f:
+        json.dump({"query": "mutation Minimize{" + query +  "}"}, f, indent=4)
+
 for i, node_id in enumerate(node_ids):
     query += """
 minimizeComment%s: minimizeComment(input:{subjectId:"%s",classifier:OUTDATED}) {minimizedComment {isMinimized}}
 """ % (i, node_id)
     if i % graphql_batch_size == 0:
-        with open(f"minimizing_query_{i//graphql_batch_size}.json", "w") as f:
-            json.dump({"query": "mutation Minimize{" + query +  "}"}, f, indent=4)
-            query = ""
+        dump_batch()
+        batch += 1
+        query = ""
+if query:
+    dump_batch()
